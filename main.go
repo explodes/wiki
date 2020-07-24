5824e3b787ecb298b09b8a4010823030bd3a7cc7
Evan Leis <evan.explodes@gmail.com>
2020-07-24 01:15:41 -0600

    [main] allow file input

--- main.go
+++ main.go
@@ -14,49 +14,67 @@ import (
 
 const (
 	generateNumberOfSuccesses = 15
-	successWords              = 7
-	timeLimit                 = 3 * time.Second
+	successWords              = 4
+	timeLimit                 = 5 * time.Second
+
+	prefixHttp  = "http://"
+	prefixHttps = "https://"
+	prefixFile  = "file://"
+
+	debugMagic = "debug"
 )
 
+type app struct {
+	r        *bufio.Reader
+	debugOut io.Writer
+}
+
 func main() {
-	term := newTerminal()
+	newApp().begin()
+}
 
-	doPage(term, "https://blog.bruce-hill.com/a-faster-weighted-random-choice")
+func newApp() *app {
+	return &app{
+		r:        bufio.NewReader(os.Stdin),
+		debugOut: ioutil.Discard,
+	}
+}
 
+func (app *app) begin() {
 	for {
-		loop(term)
+		app.loop()
 	}
 }
 
-func loop(term *terminal) {
-	url := term.prompt("Where do you want to learn from today?")
-	term.note("Ok, learning from %s...", url)
-	doPage(term, url)
+func (app *app) loop() {
+	url := app.prompt("Where do you want to learn from today?")
+	app.note("Ok, learning from %s...", url)
+	app.doPage(url)
 }
 
-func doPage(term *terminal, url string) {
+func (app *app) doPage(url string) {
 
-	resp, err := http.Get(url)
-	if err != nil {
-		term.note("Oops, something went wrong: %s", err)
+	var m *markov.Markov
+	var err error
+	if strings.HasPrefix(url, prefixHttp) || strings.HasPrefix(url, prefixHttps) {
+		m, err = app.htmlMarkov(url)
+	} else if strings.HasPrefix(url, prefixFile) {
+		m, err = app.fileMarkov(url[len(prefixFile):])
+	} else {
+		app.note("I don'app know how to handle %s", url)
 		return
 	}
-	defer mustClose(resp.Body)
-	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		term.note("Oops, something went wrong: %s", err)
+		app.note("Oops, something went wrong: %s", err)
 		return
 	}
-
-	m := markov.New(2048)
-	markov.Html(string(body)).ToMarkov(m)
 
 	success := 0
 	start := time.Now()
 	for success < generateNumberOfSuccesses {
 		now := time.Now()
 		if now.Sub(start) > timeLimit {
-			term.note("Sorry, ran out of time trying to generate new sentences.")
+			app.note("Sorry, ran out of time trying to generate new sentences.")
 			break
 		}
 		generated := m.Generate()
@@ -64,10 +82,66 @@ func doPage(term *terminal, url string) {
 			continue
 		}
 		success++
-		term.note("> %s", strings.Join(generated, " "))
+		app.note("> %s", strings.Join(generated, " "))
 	}
 
-	m.DumpStats(os.Stdout)
+	mustWrite(m.DumpStats(app.debugOut))
+}
+
+func (app *app) htmlMarkov(url string) (*markov.Markov, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer mustClose(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	m := markov.New(2048)
+	markov.Html(string(body)).ToMarkov(m)
+	return m, nil
+}
+
+func (app *app) fileMarkov(path string) (*markov.Markov, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer mustClose(f)
+	contents, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	m := markov.New(2048)
+	markov.Split(string(contents)).ToMarkov(m)
+	return m, nil
+}
+
+func (app *app) prompt(msg string) string {
+	fmt.Printf("%s: ", msg)
+	s, err := app.r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	s = s[:len(s)-1]
+
+	if s == debugMagic {
+		app.debugOut = os.Stderr
+		app.note("Debug mode enabled.")
+		return app.prompt(msg)
+	}
+
+	return s
+}
+
+func (app *app) note(format string, args ...interface{}) {
+	fmt.Println(fmt.Sprintf(format, args...))
+}
+
+func (app *app) debug(format string, args ...interface{}) {
+	mustWrite(fmt.Fprintln(app.debugOut, fmt.Sprintf(format, args...)))
 }
 
 func mustClose(c io.Closer) {
@@ -77,25 +151,8 @@ func mustClose(c io.Closer) {
 	}
 }
 
-type terminal struct {
-	r *bufio.Reader
-}
-
-func newTerminal() *terminal {
-	return &terminal{
-		r: bufio.NewReader(os.Stdin),
-	}
-}
-
-func (p *terminal) prompt(msg string) string {
-	fmt.Printf("%s: ", msg)
-	s, err := p.r.ReadString('\n')
+func mustWrite(n int, err error) {
 	if err != nil {
 		panic(err)
 	}
-	return s[:len(s)-1]
-}
-
-func (p *terminal) note(format string, args ...interface{}) {
-	fmt.Println(fmt.Sprintf(format, args...))
 }
